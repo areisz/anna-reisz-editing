b0d3d98ee35219324a16b457c70aa1011a3faa2a
Michael Reisz <michael_reisz@yahoo.com>
2021-02-15 16:28:06 -0500

    Commit initial framework

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'jekyll'
+
+group :jekyll_plugins do
+  gem 'jekyll-feed'
+  gem 'jekyll-include-cache'
+  gem 'jekyll-paginate'
+  gem 'jekyll-seo-tag'
+  gem 'jekyll-sitemap'
+end
---
+++ assets/javascript/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // document.querySelector('.fadein').appear((element) => {
+    //     const delay = element.data('delay');
+    //     setTimeout(() => {
+    //         element.addClass('appear');
+    //     }, delay);
+    // });
+
+});
