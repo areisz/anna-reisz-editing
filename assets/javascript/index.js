8a0c645aecba4605d0dc16d671126e036715a4f4
Michael Reisz <michael_reisz@yahoo.com>
2021-03-15 21:32:50 -0400

    Make some more changes to get the site set up

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // document.querySelector('.fadein').appear((element) => {
-    //     const delay = element.data('delay');
-    //     setTimeout(() => {
-    //         element.addClass('appear');
-    //     }, delay);
-    // });
+    let scrollPosition = window.scrollY;
+    const navHeader = document.querySelector('#nav-menu');
+    const headerHeight = navHeader.offsetHeight;
+
+
+    window.addEventListener('scroll', () => {
+        scrollPosition = window.scrollY;
+
+        if (scrollPosition >= headerHeight) {
+            navHeader.classList.add("minimized")
+        } 
+        
+        if (scrollPosition === 0) {
+            navHeader.classList.remove("minimized")
+        }
+    })
 
 });
